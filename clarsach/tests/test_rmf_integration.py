c17b189ce27fcd24cba8adea1c3f20a76c35f229
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2017-05-24 14:11:01 -0400

    Added readme to data folder to help keep track of the test files we have and need, also made integration test on HETG work

--- clarsach/tests/test_rmf_integration.py
+++ clarsach/tests/test_rmf_integration.py
@@ -28,11 +28,41 @@ class TestChandraACISIntegration(object):
         cls.m = cls.pl.calculate(ener_lo=cls.energ_lo, ener_hi=cls.energ_hi)
 
 
-    def test_clarsach_arf(self):
+    def test_clarsach_rmf(self):
         arf_c = ARF(self.arffile)
         rmf_c = RMF(self.rmffile)
 
         m_arf_c = arf_c.apply_arf(self.m)
-        m_rmf_c = rmf_c.appy_rmf(m_arf_c)
+        m_rmf_c = rmf_c.apply_rmf(m_arf_c)
+
+        assert np.allclose(self.sherpa_rmf, m_rmf_c)
+
+class TestChandraHETGIntegration(object):
+
+    @classmethod
+    def setup_class(cls):
+
+        cls.arffile = "data/arfs/aciss_heg1_cy19.garf"
+        cls.rmffile =  "data/rmfs/aciss_heg1_cy19.grmf"
+        cls.sherpa_rmf_file = "data/chandra_hetg_m_rmf.txt"
+
+        arf_list = fits.open(cls.arffile)
+        cls.sherpa_rmf = np.loadtxt(cls.sherpa_rmf_file)[:,1]
+
+        cls.energ_lo = arf_list[1].data.field("ENERG_LO")
+        cls.energ_hi = arf_list[1].data.field("ENERG_HI")
+
+        arf_list.close()
+
+        cls.pl = Powerlaw(norm=1.0, phoindex=2.0)
+        cls.m = cls.pl.calculate(ener_lo=cls.energ_lo, ener_hi=cls.energ_hi)
+
+
+    def test_clarsach_rmf(self):
+        arf_c = ARF(self.arffile)
+        rmf_c = RMF(self.rmffile)
+
+        m_arf_c = arf_c.apply_arf(self.m)
+        m_rmf_c = rmf_c.apply_rmf(m_arf_c*1.e5)
 
         assert np.allclose(self.sherpa_rmf, m_rmf_c)
