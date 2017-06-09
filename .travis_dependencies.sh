4f042af3a920915fece267bb115561533c608ef9
Daniela Huppenkothen <daniela.huppenkothen@nyu.edu>
2017-06-09 17:03:41 -0400

    added pytest-cov to travis depedencies

--- .travis_dependencies.sh
+++ .travis_dependencies.sh
@@ -11,7 +11,7 @@ conda_create ()
     conda update -q conda
     conda config --add channels pypi
     conda info -a
-    deps='coverage pip numpy astropy pytest'
+    deps='coverage pip numpy astropy pytest pytest-cov'
 
     conda create -q -n $ENV_NAME "python=$TRAVIS_PYTHON_VERSION" $deps
 }
